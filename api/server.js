e36921a48ff1efe6a21f6bc2540500f58e3c8d3d
chrisjul <christopher.jul@gmail.com>
2019-04-23 20:12:45 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,21 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
-const mongoose = require('mongoose');
-const config = require('./DB.js');
+//const mongoose = require('mongoose');
+//const config = require('./DB.js');
 const businessRoute = require('./business.route');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-  () => {console.log('Database is connected') },
-  err => { console.log('Can not connect to the database'+ err)}
-);
+//const productRoute = require('./product.route');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use('/business', businessRoute);
+app.use('/business', businessRoute);å
+//app.use('/product', productRoute);
 
 app.listen(PORT, function(){
   console.log('Server is running on Port:',PORT);
