d379fd7560d8059b5c5928553a634e7a5d493d7c
chrisjul <christopher.jul@gmail.com>
2019-05-02 18:37:46 +0530

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.accesskeyid = 'AKIAIF5MJU5TXKWJRMNQ';
+config.secretaccesskey = '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';
+config.bucketname = 'productdetailsinfo';
+config.key = 'productinfo.json';
+
+module.exports = config;
